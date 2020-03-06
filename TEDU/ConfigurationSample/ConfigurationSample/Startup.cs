c441f393eea9a1921dad9e841d6f6c6a45bc4dd4
Bao Lam Phuoc <bao.lam@orientsoftware.com>
2020-03-06 10:03:27 +0700

    first

--- TEDU/ConfigurationSample/ConfigurationSample/Startup.cs
+++ TEDU/ConfigurationSample/ConfigurationSample/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -12,6 +13,11 @@ namespace ConfigurationSample
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -26,15 +32,20 @@ namespace ConfigurationSample
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            // --The orders to add appsetting:
+            // privilege for use secrect
+            // depend on the env the config get when building host, app will take appsetting according to the name of environment
+            // dev env -> get appsettings.Development.json
+            // dev production -> get appsettings.Production.json
+            app.Run(async context =>
+            { 
+                await context.Response.WriteAsync(Configuration.GetSection("Message").Value); // Hello from Configuration 
+                // read child value 
+                await context.Response.WriteAsync(Configuration.GetSection("ConnectionStrings:SQLServerConnectionString").Value);// SQL Connection string sample
+                // read array
+                await context.Response.WriteAsync(Configuration.GetSection("Students:0:Name").Value); // Student A
             });
+
         }
     }
 }

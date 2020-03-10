99227fa36dfc73fc5633057eb8a61de31d6af6c0
Bao Lam Phuoc <bao.lam@orientsoftware.com>
2020-03-10 15:46:56 +0700

    first

---
+++ TEDU/DemoMVC/ASPNetCoreForms/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ASPNetCoreForms.Models;
+
+namespace ASPNetCoreForms.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(ProductEditModel model)
+        {
+            string message = string.Empty;
+            if (ModelState.IsValid)
+            {
+                //Check name
+                if (model.Name == "bao")
+                {
+                    ModelState.AddModelError("", "This name existed");
+                    return View(model);
+                }
+                message = "Product" + model.Name + ". Rate: " + model.Rate + ". Rating: " + model.Rating + " successfully";
+            }
+            else
+            {
+                return View(model);
+            }
+            return Content(message);
+        }
+    }
+}
---
+++ TEDU/DemoMVC/ASPNetCoreForms/Models/ProductEditModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNetCoreForms.Models
+{
+    public class ProductEditModel
+    {
+        [Required(ErrorMessage ="Name is required")]
+        public string Name { get; set; }
+        public decimal Rate { get; set; }
+        public int Rating { get; set; }
+    }
+}
